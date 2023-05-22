5f8e47477c6176a0179f319d7e598de1f3065c7a
KhoirulMadani <hawwari.asdhani21@gmail.com>
2023-05-22 10:01:16 +0700

    setting dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,7 +180,7 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-sun" : "uil-moon";
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
